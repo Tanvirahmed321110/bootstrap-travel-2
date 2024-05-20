7d5b3ac6892d3f0cc6a33c1c13ccd92aa94fc31d
tanvir321110 <tanvirahmed321110@gmail.com>
2024-05-20 15:24:28 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 3000, // Adjust the delay as needed
+        disableOnInteraction: false,
+    },
+    speed:1222,
+  
+    
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        // when window width is >= 960px
+        960: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
